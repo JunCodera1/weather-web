4735dbbd4608d933fa039d1de625f62366ea772b
tin <minhtien0601200@gmail.com>
2024-05-12 23:35:01 +0700

    Set up reactJS for app

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Search from './components/search';
+
+function App() {
+  return (
+    <div className="container">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
