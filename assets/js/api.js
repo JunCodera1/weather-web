7279d61824055c6bfb25238c1c2c7887b0158cca
JunCodera1 <minhtien0601200@gmail.com>
2024-05-24 12:09:27 +0700

    Full

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,39 +1,46 @@
+/**
+ * @license MIT
+ * @fileoverview All api related stuff like api_key, api request etc.
+ * @copyright Dhruva 2023 All rights reserved
+ * @author  Dhruva <dhruvabhat24@gmail.com>
+ */
 'use strict';
 
-const api_key = "bc31ea49aef35a7dd3da8a6069a039f1";
+const api_key ="e4d474a322c0877f50ad1ce9bfa13d83";
 
-/** 
- * Fetch data from server
- * @param {string} URL API url
- * @param {function} callback callback function
- **/  
+/**
+ *
+ * @param {string} URL API URL
+ * @param {Function} callback callback
+ */
 
-export const fetchData = function(URL, callback) {
+export const fetchData = function (URL, callback) {
     fetch(`${URL}&appid=${api_key}`)
-    .then(res => res.json())
-    .then(data => callback(data));
-}
-
-export const url = {
-    currentWeather(lat, lon){
-        return 
-        `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+      .then((res) => res.json())
+      .then((data) => callback(data));
+  };
+  
+  export const url = {
+    currentWeather(lat, lon) {
+      return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
     },
-    forecast(lat, lon){
-        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric&`
+    forecast(lat, lon) {
+      return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
     },
-    airPolution(lat, lon){
-        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+    airPollution(lat, lon) {
+      return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
     },
-    reverseGeo(lat, lon){
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    reverseGeo(lat, lon) {
+      return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
     },
-    /** 
+  
+    /**
      *
-     * @param {string} query Search query e.g: "London", "New York", "
-     *  
-     */ 
-    geo(query){
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
-    }
-}
+     * @param {string} query Search Query E.G.: "London", "Philadelphia"
+     * @returns
+     */
+  
+    geo(query) {
+      return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+    },
+  };
