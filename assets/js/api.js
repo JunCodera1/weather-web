0d9c082251081abc7adf770d4848c65bbd4ad446
tin <minhtien0601200@gmail.com>
2024-05-24 07:04:35 +0700

    module configuration

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,39 @@
-/**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+'use strict';
+
+const api_key = "bc31ea49aef35a7dd3da8a6069a039f1";
+
+/** 
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {function} callback callback function
+ **/  
+
+export const fetchData = function(URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+    .then(res => res.json())
+    .then(data => callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon){
+        return 
+        `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric&`
+    },
+    airPolution(lat, lon){
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+    },
+    reverseGeo(lat, lon){
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /** 
+     *
+     * @param {string} query Search query
+     *  
+     */ 
+    geo(query){
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
